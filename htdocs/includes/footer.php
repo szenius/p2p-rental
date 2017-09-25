eeb3cc3858d002f7551e06458a2c4938e8ca0d1a
Fenyee <tayfenyee@gmail.com>
2017-09-26 00:57:00 +0800

    Created login.php and included relevant folders

---
+++ htdocs/includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="diff">
+    <p class="text-center">&copy 2016 ShareStuff. All Rights Reserved | Design by <a href="https://w3layouts.com/" target="_blank">w3layouts.</a></p>
+</footer>
---
+++ htdocs/includes/header.php
@@ -0,0 +1,10 @@
+<div class="header">
+    <div class="container">
+        <div class="logo">
+            <a href="index.html"><span>Share</span>Stuff</a>
+        </div>
+        <div class="header-right">
+            <a class="account" href="login.html">My Account</a>
+        </div>
+    </div>
+</div>
