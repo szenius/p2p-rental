ddc243e561a91080d9ca78542e1425821502966f
Sze Ying Ting <32257578+szeying7@users.noreply.github.com>
2017-11-02 00:55:02 +0800

    Added search all categories (#17)

--- htdocs/functions/search_listings.sql
+++ htdocs/functions/search_listings.sql
@@ -6,9 +6,15 @@ stitle VARCHAR(256))
 RETURNS SETOF JSON AS 
 $BODY$
 BEGIN
-RETURN QUERY SELECT row_to_json(row(id, title, price, description, pickup_location, return_location, post_date, start_date, end_date, is_avail, photo, category_name, owner)) 
-FROM itemlisting il WHERE il.category_name = scategory AND il.start_date >= sstart_date AND il.end_date <= send_date
-AND (lower(il.title) LIKE '%' || lower(stitle) || '%' OR lower(il.description) LIKE '%#' || lower(stitle) || '%');
+IF lower(scategory) = "all"
+	THEN RETURN QUERY SELECT row_to_json(row(id, title, price, description, pickup_location, return_location, post_date, start_date, end_date, is_avail, photo, category_name, owner)) 
+		FROM itemlisting il WHERE il.start_date >= sstart_date AND il.end_date <= send_date
+		AND (lower(il.title) LIKE '%' || lower(stitle) || '%' OR lower(il.description) LIKE '%#' || lower(stitle) || '%');
+ELSE
+	RETURN QUERY SELECT row_to_json(row(id, title, price, description, pickup_location, return_location, post_date, start_date, end_date, is_avail, photo, category_name, owner)) 
+		FROM itemlisting il WHERE il.category_name = scategory AND il.start_date >= sstart_date AND il.end_date <= send_date
+		AND (lower(il.title) LIKE '%' || lower(stitle) || '%' OR lower(il.description) LIKE '%#' || lower(stitle) || '%');
+END IF;
 END;
 $BODY$
 LANGUAGE PLPGSQL;
