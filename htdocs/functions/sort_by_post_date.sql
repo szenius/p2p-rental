f9b76d1085c40da78502eb385d2f633f44dce209
Sze Ying Ting <32257578+szeying7@users.noreply.github.com>
2017-11-03 21:22:27 +0800

    Sort works when filters are NULL (#22)

--- htdocs/functions/sort_by_post_date.sql
+++ htdocs/functions/sort_by_post_date.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE FUNCTION sort_by_post_date(
 orderByAsc BOOLEAN,
+isUnfiltered BOOLEAN,
 scategory VARCHAR(64),
 sstart_date DATE,
 send_date DATE,
@@ -7,32 +8,37 @@ stitle VARCHAR(256))
 RETURNS SETOF JSON AS 
 $BODY$
 BEGIN
-IF lower(scategory) = 'all'
+IF isUnfiltered
 	THEN IF orderByAsc
 		THEN RETURN QUERY SELECT row_to_json(row(id, title, price, description, pickup_location, return_location, post_date, start_date, end_date, is_avail, photo, category_name, owner)) 
-			FROM itemlisting il WHERE il.start_date >= sstart_date AND il.end_date <= send_date
-			AND (lower(il.title) LIKE '%' || lower(stitle) || '%' OR lower(il.description) LIKE '%#' || lower(stitle) || '%')
-			ORDER BY il.post_date ASC;
+			FROM itemlisting il ORDER BY il.post_date ASC, il.title ASC;
 	ELSE RETURN QUERY SELECT row_to_json(row(id, title, price, description, pickup_location, return_location, post_date, start_date, end_date, is_avail, photo, category_name, owner)) 
-		FROM itemlisting il WHERE il.start_date >= sstart_date AND il.end_date <= send_date
-		AND (lower(il.title) LIKE '%' || lower(stitle) || '%' OR lower(il.description) LIKE '%#' || lower(stitle) || '%')
-		ORDER BY il.post_date DESC;
+		FROM itemlisting il ORDER BY il.post_date DESC, il.title ASC;
 	END IF;
 ELSE
-	IF orderByAsc 
-		THEN RETURN QUERY SELECT row_to_json(row(id, title, price, description, pickup_location, return_location, post_date, start_date, end_date, is_avail, photo, category_name, owner)) 
-			FROM itemlisting il WHERE il.category_name = scategory AND il.start_date >= sstart_date AND il.end_date <= send_date
-			AND (lower(il.title) LIKE '%' || lower(stitle) || '%' OR lower(il.description) LIKE '%#' || lower(stitle) || '%')
-			ORDER BY il.post_date ASC;
-	ELSE RETURN QUERY SELECT row_to_json(row(id, title, price, description, pickup_location, return_location, post_date, start_date, end_date, is_avail, photo, category_name, owner)) 
-		FROM itemlisting il WHERE il.category_name = scategory AND il.start_date >= sstart_date AND il.end_date <= send_date
-		AND (lower(il.title) LIKE '%' || lower(stitle) || '%' OR lower(il.description) LIKE '%#' || lower(stitle) || '%')
-		ORDER BY il.post_date DESC;
+	CREATE TABLE il_filtered AS SELECT * 
+		FROM itemlisting il WHERE il.start_date >= sstart_date AND il.end_date <= send_date
+		AND (lower(il.title) LIKE '%' || lower(stitle) || '%' OR lower(il.description) LIKE '%#' || lower(stitle) || '%');
+	
+	IF lower(scategory) <> 'all'
+		THEN DELETE FROM il_filtered il WHERE il.category_name <> scategory;
 	END IF;
+
+	IF orderByAsc
+		THEN RETURN QUERY SELECT row_to_json(row(id, title, price, description, pickup_location, return_location, post_date, start_date, end_date, is_avail, photo, category_name, owner)) 
+			FROM il_filtered il ORDER BY il.post_date ASC, il.title ASC;
+	ELSE RETURN QUERY SELECT row_to_json(row(id, title, price, description, pickup_location, return_location, post_date, start_date, end_date, is_avail, photo, category_name, owner))
+			FROM il_filtered il ORDER BY il.post_date DESC, il.title ASC;
+	END IF;
+
+	DROP TABLE il_filtered;
 END IF;
 END;
 $BODY$
 LANGUAGE PLPGSQL;
 
-SELECT "sort_by_post_date"(TRUE, 'Mobile Devices', '2017-05-30', '2018-01-01', 'I');
-SELECT "sort_by_post_date"(FALSE, 'Mobile Devices', '2017-05-30', '2018-01-01', 'I');
+SELECT "sort_by_post_date"(TRUE, FALSE, 'Mobile Devices', '2017-05-30', '2018-01-01', 'I');
+SELECT "sort_by_post_date"(FALSE, FALSE, 'Mobile Devices', '2017-05-30', '2018-01-01', 'I');
+SELECT "sort_by_post_date"(TRUE, TRUE, NULL, NULL, NULL, NULL);
+SELECT "sort_by_post_date"(FALSE, TRUE, NULL, NULL, NULL, NULL);
+
